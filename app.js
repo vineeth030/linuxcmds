e7ef3095b34e1c68f1fa0bca2a47cd683addf927
vineeth030 <vineeth030@gmail.com>
2017-11-23 23:03:58 +0530

    completed listing with components.

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 Vue.component('command-list', {
-    template: '<ul class="list-group"><command v-for="command in commands">{{ command.command }}</command></ul>',
+    template: `<ul class="list-group">
+                    <a v-for="(command, key) in commands" href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                        <div class="d-flex w-100 justify-content-between">
+                          <h5 :id="'command'+key" class="mb-1">{{ command.command }}</h5>
+                          <small><i class="fa fa-files-o pull-right" :data-clipboard-target="'#command'+key" aria-hidden="true"></i></small>
+                        </div>
+                       <p class="mb-1">{{ command.title }}</p>
+                    </a>
+                </ul>`,
 
     data() {
         return {
             commands: [
-                {   command: 'Aasdas asdasd', complete: true },
-                {   command: 'Basdasd asdasd', complete: false },
-                {   command: 'Casdasd asd asdasd', complete: true },
+                {   title: 'Clean cache', command: 'php bin/magento cache:flush' },
+                {   title: 'Reindex command', command: 'php bin/magento indexer:reindex' },
+                {   title: 'Deploy  static files', command: 'php bin/magento static:deploy' },
             ]
         }
     }
 });
 
-Vue.component('command', {
-    template: '<li class="list-group-item name" id="foo"><slot></slot><i class="fa fa-files-o pull-right" data-clipboard-target="#foo" aria-hidden="true"></i></li>'
-});
+
 
 var app = new Vue({
     el: '#app',
